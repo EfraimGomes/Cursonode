ae04b2e611c84b5ef93cfa459b62104c1917b5ff
Gomes <gomes.efraim@gmail.com>
2021-03-15 07:04:56 -0300

    async tools

---
+++ tools/process.js
@@ -0,0 +1,4 @@
+console.log(`Nome do Arquivo`, __filename)
+console.log(`Diretório do arquivo`, __dirname)
+console.log(`Parâmetros de execução`, process.argv)
+console.log(`Ambiente do Servidor`, process.platform)
